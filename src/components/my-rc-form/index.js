a56dff4ca3cfd7ed46ad660c22126d4570ee0055
ljmissir <ljmissir@163.com>
2020-07-26 23:30:07 +0800

    createForm

--- src/components/my-rc-form/index.js
+++ src/components/my-rc-form/index.js
@@ -1,1 +1,9 @@
-export default function createForm() {}
+import React, { Component } from "react";
+
+export default function createForm(Cmp) {
+  return class extends Component {
+    render() {
+      return <Cmp />;
+    }
+  };
+}
